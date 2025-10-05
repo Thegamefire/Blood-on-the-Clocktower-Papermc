3fcccda121ca51b031a2218a4d3fefb1c63daf5b
The game fire <thegamefire@outlook.com>
2025-10-05 18:28:02 +0200

    Now Saves VoteBlocks and VoteLevers on WorldSave and Not Only on Server Close

--- src/main/java/com/thegamefire/bloodOnTheClocktower/TokenPlaceListener.java
+++ src/main/java/com/thegamefire/bloodOnTheClocktower/TokenPlaceListener.java
@@ -1,9 +1,12 @@
 package com.thegamefire.bloodOnTheClocktower;
 
+import com.thegamefire.bloodOnTheClocktower.votes.VoteLeverListener;
+import com.thegamefire.bloodOnTheClocktower.votes.VoteManager;
 import org.bukkit.NamespacedKey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.world.WorldSaveEvent;
 
 public class TokenPlaceListener implements Listener {
     @EventHandler
@@ -11,5 +14,13 @@ public class TokenPlaceListener implements Listener {
         if (event.getItemInHand().getPersistentDataContainer().has(NamespacedKey.fromString("botc_char", BloodOnTheClocktower.instance))) {
             event.setCancelled(true);
         }
+    }
+
+
+    @EventHandler
+    public void onWorldSave(WorldSaveEvent event) {
+        //TODO Move in Code Cleanup
+        VoteManager.saveVoteBlocks();
+        VoteLeverListener.saveVoteLevers();
     }
 }
